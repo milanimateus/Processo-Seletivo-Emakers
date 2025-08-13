3f33af6425623967b300c527e6ddaf441cf5d24c
Mateus Milani Rodrigues <mateusmilanir26@gmail.com>
2025-08-13 18:55:44 -0300

    Refatoração e melhoria do componente Navbar

--- src/componentes/navbar/Navbar.jsx
+++ src/componentes/navbar/Navbar.jsx
@@ -1,39 +1,66 @@
-import React from "react";
-import imgNavbar from "../../images/navbar.png";
+// src/components/Navbar/Navbar.jsx
+
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
 import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import imgNavbar from "../../images/navbar.png";
+import "./Navbar.css";
+
+const navItens = [
+  { label: "Lançamentos", path: "/lancamentos" },
+  { label: "Populares", path: "/populares" },
+  { label: "Gêneros", path: "/generos" },
+  { label: "Promoções", path: "/promocoes" },
+  { label: "Conta", path: "/conta" },
+];
 
 const Navbar = () => {
-  const navItems = [
-    { label: "Lançamentos", path: "/lancamentos" },
-    { label: "Populares", path: "/populares" },
-    { label: "Gêneros", path: "/generos" },
-    { label: "Promoções", path: "/promocoes" },
-    { label: "Conta", path: "/conta" },
-  ];
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const closeMobileMenu = () => setIsMenuOpen(false);
 
   return (
-    <div className="navbar">
-      <div className="logo">
-        <img src={imgNavbar} alt="imagemNavbar" />
+    <nav className="navbar">
+      <Link to="/home" className="navbar__logo" onClick={closeMobileMenu}>
+        <img src={imgNavbar} alt="Game Dame Logo" />
+      </Link>
+
+      <div
+        className="navbar__toggle"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        {isMenuOpen ? <CloseIcon /> : <MenuIcon />}
       </div>
 
-      {navItems.map((item) => (
-        <div key={item.label} className="botao-navbar">
-          <Link to={item.path} className="botao">
-            {item.label}
-          </Link>
-        </div>
-      ))}
+      <ul className={isMenuOpen ? "navbar__nav active" : "navbar__nav"}>
+        {navItens.map((item) => (
+          <li key={item.path}>
+            <Link
+              to={item.path}
+              className="navbar__nav-link"
+              onClick={closeMobileMenu}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
 
-      <div className="barra-pesquisa">
-        <input type="text" />
-        <button className="icone-lupa">
+      <form className="navbar__search">
+        <input
+          type="text"
+          className="navbar__search-input"
+        />
+        <button
+          type="submit"
+          className="navbar__search-button"
+          aria-label="Pesquisar"
+        >
           <SearchIcon fontSize="inherit" />
         </button>
-      </div>
-    </div>
+      </form>
+    </nav>
   );
 };
 
